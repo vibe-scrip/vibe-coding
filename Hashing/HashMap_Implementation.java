ae72f4559692424d8612f52a0e5f42b653f3bbc5
Rushikesh <rushikesh@github.com>
2024-02-17 18:05:46 +0530

    Initial commit

---
+++ Hashing/HashMap_Implementation.java
@@ -0,0 +1,124 @@
+package Hashing;
+
+import java.util.*;
+
+public class HashMap_Implementation{
+         static class HashMap<K,V>{    // static nested class with generics class with two type params
+            private class Node{        // public class, this represents node in HashMap
+                 K key;                // variable key of type K   
+                 V value;             // variable value of type V
+                 public Node(K key, V value){   // constructor for a node class that takes param key and value   
+                       this.key = key;         // assigns the value of 'key' param to the 'key' variable of node object
+                       this.value = value;     // assigns the value of 'value' param to the 'value' variable of node object
+                 }
+            }
+         
+
+   @SuppressWarnings("unchecked")      
+   private int n;                 // nodes
+   private int N;                // buckets
+   private LinkedList<Node> buckets[];
+
+   public HashMap(){
+       this.N = 4;
+       this.buckets = new LinkedList[4];
+         for(int i=0;i<4;i++){
+            this.buckets[i] = new LinkedList<>();
+         }
+   }
+
+   private int hashFunction(K key){
+           int bi = key.hashCode();
+           return Math.abs(bi) % N;   
+        }
+
+   private int searchInLL(K key, int bi){
+           LinkedList<Node> ll = buckets[bi];
+           for(int i =0 ;i < ll.size(); i++)
+           {
+             if(ll.get(i).key == key){
+                  return i;
+             }
+           }
+           return -1;
+   }     
+
+   @SuppressWarnings("unchecked")
+   private void rehash(){
+          LinkedList<Node> oldBucket[] = buckets;
+          buckets = new LinkedList[N * 2];
+          for(int i = 0 ; i < N*2 ; i++){
+               buckets[i] = new LinkedList<>();
+          } 
+         
+          for(int i = 0 ; i < oldBucket.length ; i++){
+                LinkedList<Node> ll = oldBucket[i];
+                for(int j = 0 ; j < ll.size() ; j++){
+                        Node node = ll.get(j);
+                        put(node.key, node.value);
+                }
+          }
+   }
+
+   public void put(K key, V value){
+        int bi = hashFunction(key);
+        int di = searchInLL(key,bi);
+        if(di == -1){
+             buckets[bi].add(new Node(key, value));
+             n++;
+        }
+        else{
+            Node node = buckets[bi].get(di);
+            node.value = value;
+        }
+
+        double lambda = (double)n/N;
+        
+        if(lambda > 2.0){
+            rehash();
+        }
+
+   }
+
+   public V get(K key){
+    int bi = hashFunction(key);
+    int di = searchInLL(key,bi);
+    if(di == -1){
+         return null;
+    }
+    else{
+        Node node = buckets[bi].get(di);
+        return node.value;
+    }
+   }
+
+   public ArrayList<K> keySet(){
+         ArrayList<K> keys = new ArrayList<>();
+         for(int i = 0 ; i < buckets.length ; i++){
+            LinkedList<Node> ll = buckets[i];
+            for(int j = 0 ; j < ll.size() ; j++){
+                Node node = ll.get(j);
+                keys.add(node.key);
+            }
+         }
+         return keys;
+   }
+
+}
+
+
+
+public static void main(String[] args){
+        HashMap<Integer,String> map = new HashMap<>();
+        map.put(1,"India");
+        map.put(2, "Afghanistan");
+        map.put(5,"Russia");
+        map.put(3,"Mexico");
+        map.put(4,"Pakistan");
+
+        ArrayList<Integer> keys = map.keySet();
+        for(int i = 0; i < keys.size() ; i++){
+               System.out.println(keys.get(i) + " " + map.get(keys.get(i)));   
+        }
+}
+}
